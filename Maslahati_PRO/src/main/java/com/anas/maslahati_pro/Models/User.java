d988906cdc2d189267fc703422958bf1ba6a1b70
Anas hawwash <hawwashanas@gmail.com>
2025-11-27 14:20:17 +0200

    AJAX

--- Maslahati_PRO/src/main/java/com/anas/maslahati_pro/Models/User.java
+++ Maslahati_PRO/src/main/java/com/anas/maslahati_pro/Models/User.java
@@ -50,6 +50,7 @@ public class User {
 
     private boolean craftsman = false;
 
+
 //  One user can review More than one services
     @OneToMany(mappedBy = "reviewer", cascade = CascadeType.ALL)
     private List<Review> reviews = new ArrayList<>();
